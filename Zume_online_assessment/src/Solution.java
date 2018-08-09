725e4ab5b5704cb94a3a99c498afd1e3c4d99fb5
ericpoof <ericpoof@gmail.com>
2018-08-08 23:41:50 -0700

    Zume online assessment: Json parse with org.json.JSONArray, JSONObject

--- Zume_online_assessment/src/Solution.java
+++ Zume_online_assessment/src/Solution.java
@@ -5,6 +5,9 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.HashMap;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 public class Solution {
 	 public static void main(String[] args) throws Exception{
@@ -39,10 +42,60 @@ public class Solution {
  
 		while ((line = in.readLine()) != null) {
 			response.append(line);
+			System.out.println("line " + line);
 		}
 		in.close();
  
 		System.out.println(response.toString());
+		JSONObject myResponse = new JSONObject(response.toString());
+		JSONArray data =  myResponse.getJSONArray("data");
+		int total_record = myResponse.getInt("total");
+		int total_pages = myResponse.getInt("total_pages");
+		System.out.println("total record " + total_record);
+		//for (JSONObject item : data) {
+		for (int i=0;i<data.length();i++){
+			JSONObject item = data.getJSONObject(i);
+			System.out.println("title " + i + " " +    item.getString("Title"));
+
+		}
+		if (total_pages > 1) {
+			for (int i=1;i<total_pages;i++) {
+				sb = new StringBuilder("https://jsonmock.hackerrank.com/api/movies/search/");
+				sb.append("?Title=");
+				sb.append(substr);
+				sb.append("&");
+				sb.append("page=");
+				sb.append(i+1);
+				System.out.println("url" + sb.toString());
+				obj = new URL(sb.toString());
+				con = (HttpURLConnection) obj.openConnection();
+				con.setRequestMethod("GET");
+				System.out.println("Response Code : " + con.getResponseCode());
+				System.out.println("Response Message : " + con.getResponseMessage());
+				in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				response = new StringBuffer();
+		 
+				while ((line = in.readLine()) != null) {
+					response.append(line);
+					System.out.println("line " + line);
+				}
+				in.close();
+		 
+				System.out.println(response.toString());
+				myResponse = new JSONObject(response.toString());
+				data =  myResponse.getJSONArray("data");
+				total_record = myResponse.getInt("total");
+				System.out.println("total record " + total_record);
+				System.out.println("data length " + data.length());
+				//for (JSONObject item : data) {
+				for (int j=0;j<data.length();j++){
+					JSONObject item = data.getJSONObject(j);
+					System.out.println("title " + j + " " +    item.getString("Title"));
+
+				}
+				
+			}
+		}
 
 
 		
